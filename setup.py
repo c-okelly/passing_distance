46717cf6008dd8f22d3cee5876b5745581fcf01b
c-okelly <conor.okelly@murex.com>
2017-12-01 15:17:36 +0000

    Fill out setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='passing_distance',
+		version='0.1',
+		description='Passing distance cycling project',
+		url='https://github.com/c-okelly/passing_distance',
+		author="Conor O'Kelly",
+		author_email='okellyconor@gmail.com',
+		license='MIT',
+		packages=[],
+		zip_safe=False)
